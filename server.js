de7ef08c68e87e8e5045917723d075b9160afe09
mint673 <mminting@student.ubc.ca>
2022-01-11 22:12:01 -0800

    finished minimum functionality with ugly front-end

--- server.js
+++ server.js
@@ -4,36 +4,48 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
 // set up body-parser
 app.use(parser.urlencoded({extended: true}));
 
 // connect to mongo
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://127.0.0.1/db', {useNewUrlParser: true});
 
+// set up person model
 // !!! add photo
 const personSchema = new mongoose.Schema({
     name: String,
     contact: String,
-    sports: {
-        sport: String,
-        level: String
-    }
+    sports: String
 });
   
 const Person = mongoose.model('Person', personSchema);
+let listOfPerson;
 
-// mongo crud with mongoose
-
+// home page
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+    Person.find({}, function(err, people){
+        listOfPerson = people;
+        res.render('index.ejs', {persons: listOfPerson});
+        console.log(listOfPerson);
+    })
 })
 
 // create a user
 app.post('/', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
+    const person = new Person({ 
+        name: req.body.name,
+        contact: req.body.contact,
+        sports: req.body.sports
+    });
+    Person.insertMany([person]);
+    res.redirect('/');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
 
