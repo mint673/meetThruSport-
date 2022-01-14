e0b39067db149b86b677444c3a853f81b58db552
mint673 <mminting@student.ubc.ca>
2022-01-13 19:59:03 -0800

    added autocomplete for sport input

---
+++ public/js/js.js
@@ -0,0 +1,33 @@
+import {suggestions} from "./suggestions.js";
+const addSportButton = document.getElementById("add-sport-button");
+const listOfSport = document.getElementById("list-of-sport");
+
+// add sport btn
+let numSports = 1;
+addSportButton.onclick = (ev => {
+    numSports++;
+    let sportField = document.createElement('li');
+    sportField.innerHTML = 
+    '<li><input type="text" name="sport" value="sport"> <input type="text" name="level" value="skill level">\n <div class="remove-sport-btn">remove</div>';
+    listOfSport.appendChild(sportField);
+})
+
+// auto-complete
+const autoCompleteJS = new autoComplete({
+    placeHolder: "Search for Food...",
+    data: {
+        src: suggestions.sort(),
+        cache: true,
+    },
+    resultItem: {
+        highlight: true
+    },
+    events: {
+        input: {
+            selection: (event) => {
+                const selection = event.detail.selection.value;
+                autoCompleteJS.input.value = selection;
+            }
+        }
+    }
+});
